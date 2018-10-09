984b244054b71d3afa3e9dfc65fb25623db61cbc
jrajan20 <jai_20j@hotmail.com>
2018-10-09 23:38:04 +0000

    Creating tweets complete

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+  
+
+   
+  	$(".text-box").bind('input propertychange',function(){
+  		
+  		let i = 140 - $(this).val().length;	
+  		$(".counter").text(i);
+  		console.log(i);
+  		if (i < 0){
+  		$('.counter').css('color','red');
+  		}
+  		else{
+  		$('.counter').css('color','black');
+  		}
+ 	 });
+
+
+  	
+  			
+        	
+
+  
+});
